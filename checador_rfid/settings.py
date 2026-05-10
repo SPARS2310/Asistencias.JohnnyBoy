808ac595c64be7446ac275a58ac86ee5ece5450a
SPARS2310 <spars0105@gmail.com>
2026-05-10 16:31:40 -0700

    Limpieza total de codigo

--- checador_rfid/settings.py
+++ checador_rfid/settings.py
@@ -25,11 +25,8 @@ SECRET_KEY = 'django-insecure-yh!bpjx1x0i*u(s*29w7%lje&a)=lqv&$l9w1p-y!@=0j&8ium
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ... (deja lo anterior igual hasta ALLOWED_HOSTS)
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://asistenciasjohnnyboy-production.up.railway.app']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -38,21 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'asistencia',
+    'asistencia', 
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-ROOT_URLCONF = 'checador_rfid.urls'
-
+# ... (busca la sección TEMPLATES y reemplázala completa)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -69,52 +55,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'checador_rfid.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/6.0/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+# ... (ve al final del archivo y agrega esto)
 STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://asistenciasjohnnyboy-production.up.railway.app']
