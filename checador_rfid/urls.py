e8775175cdbe24de552ede1616f500dbfc7f031e
SPARS2310 <spars0105@gmail.com>
2026-05-10 15:42:54 -0700

    Corrigiendo urls

--- checador_rfid/urls.py
+++ checador_rfid/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from asistencia.views import registrar_chequeo
+from asistencia.views import registrar_chequeo, pantalla_principal
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Esta es la URL a la que tu hardware enviará los datos:
     path('api/registrar/', registrar_chequeo, name='registrar_chequeo'),
+    path('', pantalla_principal, name='pantalla_principal'),
 ]
