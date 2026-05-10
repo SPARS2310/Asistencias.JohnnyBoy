ea861c645d0cfcae76855c936ef2fd2f117929ae
SPARS2310 <spars0105@gmail.com>
2026-05-10 16:02:32 -0700

    Activando tablas en el panel de admin

--- asistencia/admin.py
+++ asistencia/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Empleado, Registro
 
-# Register your models here.
+@admin.register(Empleado)
+class EmpleadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'uid_rfid')
+    search_fields = ('nombre', 'uid_rfid')
+
+@admin.register(Registro)
+class RegistroAdmin(admin.ModelAdmin):
+    list_display = ('empleado', 'tipo', 'fecha_hora')
+    list_filter = ('tipo', 'fecha_hora')
